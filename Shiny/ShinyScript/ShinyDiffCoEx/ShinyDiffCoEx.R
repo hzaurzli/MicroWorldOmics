4c0ec7dfff8828b2b90d56d1fc612dcaba683194
Small runze <yun_act@163.com>
2024-03-28 16:56:56 +0800

    Update ShinyDiffCoEx.R

--- Shiny/ShinyScript/ShinyDiffCoEx/ShinyDiffCoEx.R
+++ Shiny/ShinyScript/ShinyDiffCoEx/ShinyDiffCoEx.R
@@ -19,8 +19,13 @@ ui <- tagList(
         actionButton('reset', 'RESET'),
         actionButton('start', 'START'),
         hr(),
-        downloadButton("downloadData", "Download Table"),
-        hr(),
+        h4(strong('Download all results (large data be patient)')),
+        downloadButton("downloadData", "Download dissimilarity table"),
+        br(),
+        br(),
+        downloadButton("downloadData1", "Download correlation matrix"),
+        br(),
+        br(),
         downloadButton("plotDown", "Download figure"),
         br(),
         br(),
@@ -41,7 +46,7 @@ ui <- tagList(
         br(),
         h5(strong("Larger T values represent that the weights (regulatory relationships) of gene pairs are less different between the two treatments!!!")),
         hr(),
-        plotOutput(outputId = "detectfig")
+        plotOutput(outputId = "detectfig", width = "90%")
       )
     )
   )
@@ -112,63 +117,107 @@ server <- function(input, output, session) {
       warning("Please upload files!")
     }
     else{
-        # 定义 β 值
-        beta1=input$num
+      # 定义 β 值
+      beta1=input$num
+      
+      name1 = colnames(otuC1_dat)
+      name2 = colnames(otuC2_dat)
+      
+      name_i1 = name1[order(name1)]
+      name_i2 = name2[order(name2)]
+      
+      if (!identical(name_i1, name_i2)) {
+        message('Error: different column names for documents 1 and 2')
+      }
+      else{
+        datC1 = otuC1_dat
+        datC2 = otuC2_dat
         
-        name1 = colnames(otuC1_dat)
-        name2 = colnames(otuC2_dat)
-		
-		name_i1 = name1[order(name1)]
-		name_i2 = name2[order(name2)]
-        
-        if (!identical(name_i1, name_i2)) {
-          message('Error: different column names for documents 1 and 2')
-        }
-        else{
-		  datC1 = otuC1_dat
-		  datC2 = otuC2_dat
-		  
-          output$table <- renderDataTable({
-            AdjMatC1<-sign(cor(datC1,method="spearman"))*(cor(datC1,method="spearman"))^2
-            AdjMatC2<-sign(cor(datC2,method="spearman"))*(cor(datC2,method="spearman"))^2
-            
-            diag(AdjMatC1)<-0
-            diag(AdjMatC2)<-0
-            collectGarbage()
-            # 计算相异矩阵 T
-            dissTOMC1C2=TOMdist((abs(AdjMatC1-AdjMatC2)/2)^(beta1/2))
-            collectGarbage()
-            
-            geneTreeC1C2 <<- flashClust(as.dist(dissTOMC1C2), method = "average");
-            
-            dynamicModsHybridC1C2 = cutreeDynamic(dendro = geneTreeC1C2, distM = dissTOMC1C2,method="hybrid",cutHeight=.996,deepSplit = T, pamRespectsDendro = FALSE,minClusterSize = 20);
-            
-            #Every module is assigned a color. Note that GREY is reserved for genes which do not belong to any differentially coexpressed module
-            dynamicColorsHybridC1C2 = labels2colors(dynamicModsHybridC1C2)
-            
-            #the next step merges clusters which are close (see WGCNA package documentation)
-            mergedColorC1C2<-mergeCloseModules(rbind(datC1,datC2),dynamicColorsHybridC1C2,cutHeight=.2)$color
-            colorh1C1C2 <<- mergedColorC1C2
-            
-            myAdjacencyMatrix = as.matrix(dissTOMC1C2)
-            colnames(myAdjacencyMatrix) = name1
-            g  <- graph.adjacency(myAdjacencyMatrix,weighted=TRUE)
-            df <- get.data.frame(g)
-            colnames(df) = c('from','to','T value')
-            df_dat <<- df
-            
-          },options = list(pageLength = 10))
-        }
-  
+        output$table <- renderDataTable({
+          AdjMatC1<-sign(cor(datC1,method="spearman"))*(cor(datC1,method="spearman"))^2
+          AdjMatC2<-sign(cor(datC2,method="spearman"))*(cor(datC2,method="spearman"))^2
+          
+          corC1 = cor(datC1,method="spearman")
+          corC2 = cor(datC2,method="spearman")
+          
+          corC1_dat = corC1
+          corC2_dat = corC2
+          
+          corC1_dat[upper.tri(corC1_dat,diag = T)] <- 0
+          corC2_dat[upper.tri(corC2_dat,diag = F)] <- 0
+          
+          cor_dat = corC1_dat + corC2_dat
+          cor_final <<- cor_dat
+          
+          diag(AdjMatC1)<-0
+          diag(AdjMatC2)<-0
+          collectGarbage()
+          # 计算相异矩阵 T
+          dissTOMC1C2=TOMdist((abs(AdjMatC1-AdjMatC2)/2)^(beta1/2))
+          collectGarbage()
+          
+          geneTreeC1C2 <<- flashClust(as.dist(dissTOMC1C2), method = "average");
+          
+          dynamicModsHybridC1C2 = cutreeDynamic(dendro = geneTreeC1C2, distM = dissTOMC1C2,method="hybrid",cutHeight=.996,deepSplit = T, pamRespectsDendro = FALSE,minClusterSize = 20);
+          
+          #Every module is assigned a color. Note that GREY is reserved for genes which do not belong to any differentially coexpressed module
+          dynamicColorsHybridC1C2 = labels2colors(dynamicModsHybridC1C2)
+          
+          #the next step merges clusters which are close (see WGCNA package documentation)
+          mergedColorC1C2<-mergeCloseModules(rbind(datC1,datC2),dynamicColorsHybridC1C2,cutHeight=.2)$color
+          colorh1C1C2 <<- mergedColorC1C2
+          
+          
+          item = names(table(colorh1C1C2))
+          item_order = c()
+          ann = c()
+          num = 1
+          for (i in item) {
+            item_order = c(item_order,which(colorh1C1C2 == i))
+            ann = c(ann,rep(paste('M',num,sep = ''),
+                            length(which(colorh1C1C2 == i))))
+            num = num + 1
+          }
+          
+          datC1 = datC1[,item_order]
+          datC2 = datC2[,item_order]
+          
+          ann_row = data.frame(ann)
+          row.names(ann_row) = colnames(datC1)
+          ann_row_final <<- ann_row
+          
+          corC1 = cor(datC1,method="spearman")
+          corC2 = cor(datC2,method="spearman")
+          
+          corC1_dat = corC1
+          corC2_dat = corC2
+          
+          corC1_dat[upper.tri(corC1_dat,diag = T)] <- 0
+          corC2_dat[lower.tri(corC2_dat,diag = F)] <- 0
+          
+          cor_dat = corC1_dat + corC2_dat
+          cor_final <<- cor_dat
+          
+          
+          myAdjacencyMatrix = as.matrix(dissTOMC1C2)
+          colnames(myAdjacencyMatrix) = name1
+          g  <- graph.adjacency(myAdjacencyMatrix,weighted=TRUE)
+          df <- get.data.frame(g)
+          colnames(df) = c('from','to','T value')
+          df_dat <<- df
+          
+        },options = list(pageLength = 10))
+      }
+      
       
       output$detectfig <- renderPlot({
-        plotDendroAndColors(geneTreeC1C2, colorh1C1C2, "Hybrid Tree Cut",
-                            dendroLabels = FALSE, hang = 0.03,
-                            addGuide = TRUE, guideHang = 0.05,
-                            main = "ASV/OTU dendrogram and module")
+        pheatmap::pheatmap(cor_final,cluster_cols = F,
+                           cluster_rows = F,show_rownames = F,
+                           show_colnames = F,border_color = NA,
+                           annotation_row = ann_row_final,
+                           annotation_legend = T)
         
       })
-      
     }
   })
   
@@ -179,6 +228,16 @@ server <- function(input, output, session) {
     },
     content = function(file) {
       write.csv(df_dat,file,row.names = T,quote = F)
+    }
+  )
+  
+  
+  output$downloadData1 <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(cor_final,file,row.names = T,quote = F)
     }
   )
   
@@ -194,10 +253,11 @@ server <- function(input, output, session) {
       }else{
         jpeg(file)
       }
-      plotDendroAndColors(geneTreeC1C2, colorh1C1C2, "Hybrid Tree Cut",
-                          dendroLabels = FALSE, hang = 0.03,
-                          addGuide = TRUE, guideHang = 0.05,
-                          main = "ASV/OTU dendrogram and module colors cells")
+      pheatmap::pheatmap(cor_final,cluster_cols = F,
+                         cluster_rows = F,show_rownames = F,
+                         show_colnames = F,border_color = NA,
+                         annotation_row = ann_row_final,
+                         annotation_legend = T)
       dev.off()
     }
   )
